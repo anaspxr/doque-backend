567114a29fd8f58cd3efb5f71dcfdda1e959547b
Sinan <sinanbh9605@gmail.com>
2024-11-23 12:16:47 +0530

    chatController updated (#27)

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,5 @@
 import express from "express";
-import {
-  getAllUsers,
-  getUserById,
-  taskAssignedToUser,
-  updatedUserProfile,
-} from "../controllers/userController";
+import { getAllUsers, getUserById, taskAssignedToUser, updatedUserProfile } from "../controllers/userController";
 import { verifyToken } from "../middlewares/verifyToken";
 import { errorCatch } from "../utils/error/errorCatch";
 import { editUserDetails } from "../utils/zodSchemas";
@@ -13,17 +8,8 @@ import { validateData } from "../middlewares/zodValidation";
 const router = express.Router();
 
 router.get("/:userId", verifyToken, errorCatch(getUserById));
-router.put(
-  "/:userId",
-  verifyToken,
-  validateData(editUserDetails),
-  errorCatch(updatedUserProfile)
-);
+router.put("/:userId", verifyToken, validateData(editUserDetails), errorCatch(updatedUserProfile));
 router.get("/", verifyToken, errorCatch(getAllUsers));
-router.get(
-  "/:userId/assigned-tasks",
-  verifyToken,
-  errorCatch(taskAssignedToUser)
-);
+router.get("/:userId/assigned-tasks", verifyToken, errorCatch(taskAssignedToUser));
 
 export default router;
